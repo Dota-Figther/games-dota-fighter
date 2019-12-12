6c87a855678859c4c53180d71ba16e4c564b93ac
Danang Putra Bahari <danangbahari89@gmail.com>
2019-12-12 20:41:37 +0700

    create room

---
+++ client/src/views/Lobby.vue
@@ -0,0 +1,37 @@
+<template>
+<div>
+  <input type="text" placeholder="room name" id="room-name" v-name="">
+  <button class="btn btn-primary">Create Room</button>
+  <cardroom v-for="(data,index) in listRoom" :key="index" :room="data"></cardroom>
+</div>
+</template>
+
+<script>
+
+import cardroom from '../components/CardRoom'
+
+export default {
+  name: 'lobby',
+  data () {
+    return {
+      listRoom: []
+    }
+  },
+  components: {
+    cardroom
+  },
+  methods: {
+    getData () {
+      this.$store.dispatch('getRoomData')
+    }
+  },
+  created () {
+    this.getData()
+    this.listRoom = this.$store.state.listRoom
+  }
+}
+</script>
+
+<style>
+
+</style>
