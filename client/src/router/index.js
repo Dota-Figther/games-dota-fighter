3adb65877107b61c0b84b988beb1edb44917757b
Richard Khonan <richardkhonan0797@gmail.com>
2019-12-12 20:06:56 +0700

    choose character

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+// import Character from '../views/ChooseChar.vue'
 
 Vue.use(VueRouter)
 
@@ -17,7 +18,12 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
+  // {
+  //   path: '/character',
+  //   name: 'character',
+  //   component: Character
+  // }
 ]
 
 const router = new VueRouter({
