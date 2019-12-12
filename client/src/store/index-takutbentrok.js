10a55659117ff1cb1ad8991078b34c46781d514a
Danang Putra Bahari <danangbahari89@gmail.com>
2019-12-12 20:49:40 +0700

    gak tau kenapa

--- client/src/store/index-takutbentrok.js
+++ client/src/store/index-takutbentrok.js
@@ -5,38 +5,38 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    heroList : [
+    heroList: [
       {
-        name : 'morphling',
-        health : 10000,
-        basicAttack : 1000,
-        skillAttack : 1300,
-        image : '',
-        thumbnail : ''
+        name: 'morphling',
+        health: 10000,
+        basicAttack: 1000,
+        skillAttack: 1300,
+        image: '',
+        thumbnail: ''
       },
       {
-        name : 'earth spirit',
-        health : 12000,
-        basicAttack : 900,
-        skillAttack : 1000,
-        image : '',
-        thumbnail : ''
+        name: 'earth spirit',
+        health: 12000,
+        basicAttack: 900,
+        skillAttack: 1000,
+        image: '',
+        thumbnail: ''
       },
       {
-        name : 'ember spirit',
-        health : 10000,
-        basicAttack : 1100,
-        skillAttack : 1200,
-        image : '',
-        thumbnail : ''
+        name: 'ember spirit',
+        health: 10000,
+        basicAttack: 1100,
+        skillAttack: 1200,
+        image: '',
+        thumbnail: ''
       },
       {
-        name : 'invoker',
-        health : 11000,
-        basicAttack : 800,
-        skillAttack : 1300,
-        image : '',
-        thumbnail : ''
+        name: 'invoker',
+        health: 11000,
+        basicAttack: 800,
+        skillAttack: 1300,
+        image: '',
+        thumbnail: ''
       }
     ]
   },
@@ -44,12 +44,12 @@ export default new Vuex.Store({
 
   },
   actions: {
-    chooseHero({ state, commit }, payload){
+    chooseHero ({ state, commit }, payload) {
       let obj = {}
       obj[payload.user] = state.heroList[payload.hero]
       db.collection('dotaFighter')
         .doc(payload.room)
-        .set( obj )
+        .set(obj)
         .then(() => {
           commit('')
         })
