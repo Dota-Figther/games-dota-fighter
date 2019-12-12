354a24ea0ba7a2e1fd0c37c74304031a2b730fa4
Danang Putra Bahari <danangbahari89@gmail.com>
2019-12-12 22:45:59 +0700

    edit room on progress

--- client/src/store/index-takutbentrok.js
+++ client/src/store/index-takutbentrok.js
@@ -1,61 +1,61 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+// import Vue from 'vue'
+// import Vuex from 'vuex'
 
-Vue.use(Vuex)
+// Vue.use(Vuex)
 
-export default new Vuex.Store({
-  state: {
-    heroList : [
-      {
-        name : 'morphling',
-        health : 10000,
-        basicAttack : 1000,
-        skillAttack : 1300,
-        image : '',
-        thumbnail : ''
-      },
-      {
-        name : 'earth spirit',
-        health : 12000,
-        basicAttack : 900,
-        skillAttack : 1000,
-        image : '',
-        thumbnail : ''
-      },
-      {
-        name : 'ember spirit',
-        health : 10000,
-        basicAttack : 1100,
-        skillAttack : 1200,
-        image : '',
-        thumbnail : ''
-      },
-      {
-        name : 'invoker',
-        health : 11000,
-        basicAttack : 800,
-        skillAttack : 1300,
-        image : '',
-        thumbnail : ''
-      }
-    ]
-  },
-  mutations: {
+// export default new Vuex.Store({
+//   state: {
+//     heroList : [
+//       {
+//         name : 'morphling',
+//         health : 10000,
+//         basicAttack : 1000,
+//         skillAttack : 1300,
+//         image : '',
+//         thumbnail : ''
+//       },
+//       {
+//         name : 'earth spirit',
+//         health : 12000,
+//         basicAttack : 900,
+//         skillAttack : 1000,
+//         image : '',
+//         thumbnail : ''
+//       },
+//       {
+//         name : 'ember spirit',
+//         health : 10000,
+//         basicAttack : 1100,
+//         skillAttack : 1200,
+//         image : '',
+//         thumbnail : ''
+//       },
+//       {
+//         name : 'invoker',
+//         health : 11000,
+//         basicAttack : 800,
+//         skillAttack : 1300,
+//         image : '',
+//         thumbnail : ''
+//       }
+//     ]
+//   },
+//   mutations: {
 
-  },
-  actions: {
-    chooseHero({ state, commit }, payload){
-      let obj = {}
-      obj[payload.user] = state.heroList[payload.hero]
-      db.collection('dotaFighter')
-        .doc(payload.room)
-        .set( obj )
-        .then(() => {
-          commit('')
-        })
-        .catch(console.log)
-    }
-  },
-  modules: {
-  }
-})
+//   },
+//   actions: {
+//     chooseHero({ state, commit }, payload){
+//       let obj = {}
+//       obj[payload.user] = state.heroList[payload.hero]
+//       db.collection('dotaFighter')
+//         .doc(payload.room)
+//         .set( obj )
+//         .then(() => {
+//           commit('')
+//         })
+//         .catch(console.log)
+//     }
+//   },
+//   modules: {
+//   }
+// })
--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,18 +1,59 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import db from '../../config/firestore'
+import db from '../config/firestore'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    listRoom: []
+    listRoom: [],
+    heroList: [
+      {
+        name: 'morphling',
+        health: 10000,
+        basicAttack: 1000,
+        skillAttack: 1300,
+        defense: 2000,
+        image: '',
+        thumbnail: ''
+      },
+      {
+        name: 'earth spirit',
+        health: 12000,
+        basicAttack: 900,
+        skillAttack: 1000,
+        defense: 1500,
+        image: '',
+        thumbnail: ''
+      },
+      {
+        name: 'ember spirit',
+        health: 10000,
+        basicAttack: 1100,
+        skillAttack: 1200,
+        defense: 1300,
+        image: '',
+        thumbnail: ''
+      },
+      {
+        name: 'invoker',
+        health: 11000,
+        basicAttack: 800,
+        skillAttack: 1300,
+        defense: 1400,
+        image: '',
+        thumbnail: ''
+      }
+    ]
   },
   mutations: {
+    SET_LIST_ROOM (state, payload) {
+      state.listRoom = payload
+    }
   },
   actions: {
     getRoomData ({ state, commit }) {
-      db.collection('dotaFigther').onSnapShot(function (querySnapshot) {
+      db.collection('dotaFighter').onSnapshot(function (querySnapshot) {
         let rooms = []
         querySnapshot.forEach(function (doc) {
           let obj = {
@@ -21,9 +62,27 @@ export default new Vuex.Store({
           }
           rooms.push(obj)
         })
-        console.log(rooms)
-        state.listRoom = rooms
+        commit('SET_LIST_ROOM', rooms)
       })
+    },
+    createRoom ({ commit, dispatch }, payload) {
+      let player = payload.user
+      let data = {
+        username: player,
+        attack: 0,
+        defense: 0,
+        hero: '',
+        health: 0,
+        skill: 0
+      }
+      db.collection('dotaFighter').doc(payload.room).add(data)
+        .then(function () {
+          console.log('Document successfully written!')
+          dispatch('getRoomData')
+        })
+        .catch(function (error) {
+          console.error('Error writing document: ', error)
+        })
     }
   },
   modules: {
