4c1eba1643650013297e4e3572d892c66861b7d5
SYX <Syirilrakhulhaqim@gmail.com>
2019-12-13 15:35:15 +0700

    Sync

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,40 +1,44 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Vue from "vue";
+import VueRouter from "vue-router";
 // import Character from '../views/ChooseChar.vue'
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'homepage',
+    path: "/",
+    name: "homepage",
     component: () =>
-      import(/* webpackChunkName: "about" */ '../views/HomePage.vue')
+      import(/* webpackChunkName: "about" */ "../views/HomePage.vue")
   },
   {
-    path: '/lobby',
-    name: 'lobby',
-    component: () => import(/* webpackChunkName: "about" */ '../views/Lobby.vue'),
+    path: "/lobby",
+    name: "lobby",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Lobby.vue"),
     children: [
       {
-        path: ':room',
-        name: 'room',
-        component: () => import(/* webpackChunkName: "room" */ '../views/ChooseChar.vue')
+        path: ":room",
+        name: "room",
+        component: () =>
+          import(/* webpackChunkName: "room" */ "../views/ChooseChar.vue"),
+        children: [
+          {
+            path: "fight",
+            name: "fight",
+            component: () =>
+              import(/* webpackChunkName: "about" */ "../views/FightPage.vue")
+          }
+        ]
       }
     ]
-  },
-  {
-    path: '/fight',
-    name: 'fight',
-    component: () =>
-      import(/* webpackChunkName: "about" */ '../views/FightPage.vue')
   }
-]
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
   routes
-})
+});
 
-export default router
+export default router;
